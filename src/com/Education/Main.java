87ab6d74ed3a4553c1e6c0461a39da5753ad4a6e
Artem <artym.surkov@mail.ru>
2022-03-28 10:11:22 +0500

    Task 13. Roman to Integer

--- src/com/Education/Main.java
+++ src/com/Education/Main.java
@@ -3,17 +3,8 @@ package com.Education;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {0,2,1,5,3,4};
-        for (int i=0; i<nums.length; i++){
-            System.out.println(nums[nums[i]]);
-        }
+        //LeetCode Task 13. Roman to Integer
+        String s = "MCMXCIV";
+        System.out.println(stringTask.romanToInt(s));
      }
-
-    public static int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-        for (int i = 0; i<nums.length; i++){
-            ans[i] = nums[nums[i]];
-        }
-        return ans;
-    }
 }
---
+++ src/com/Education/stringTask.java
@@ -0,0 +1,31 @@
+package com.Education;
+
+import java.util.HashMap;
+
+public class stringTask {
+    /*
+         LeetCode Task 13. Roman to Integer
+    */
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prev = 0;
+
+        HashMap<String, Integer> dictRoman = new HashMap<>();
+        dictRoman.put("I", 1);
+        dictRoman.put("V", 5);
+        dictRoman.put("X", 10);
+        dictRoman.put("L", 50);
+        dictRoman.put("C", 100);
+        dictRoman.put("D", 500);
+        dictRoman.put("M", 1000);
+
+        for(int i = s.length()-1; i >= 0; i--){
+            int cur = dictRoman.get(Character.valueOf(s.charAt(i)).toString());
+            if (cur < prev) result -= cur;
+            else result += cur;
+            prev = cur;
+        }
+
+        return result;
+    }
+}
