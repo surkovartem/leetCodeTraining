358ff0d102d7964b52f75ede8eb1eaf3dd5e8b43
Artem <artym.surkov@mail.ru>
2022-03-28 10:22:45 +0500

    import static System

--- src/com/Education/Main.java
+++ src/com/Education/Main.java
@@ -1,10 +1,11 @@
 package com.Education;
+import static java.lang.System.*;
 
 public class Main {
 
     public static void main(String[] args) {
         //LeetCode Task 13. Roman to Integer
         String s = "MCMXCIV";
-        System.out.println(stringTask.romanToInt(s));
+        out.println(stringTask.romanToInt(s));
      }
 }
