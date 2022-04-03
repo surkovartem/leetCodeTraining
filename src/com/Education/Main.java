ba5bafb7b4928ae27cbe63009a454a2b7503051b
Artem <artym.surkov@mail.ru>
2022-04-03 08:42:58 +0500

    //LeetCode Task 21. Merge Two Sorted Lists

--- src/com/Education/Main.java
+++ src/com/Education/Main.java
@@ -1,9 +1,12 @@
 package com.Education;
+import java.util.LinkedList;
+
 import static java.lang.System.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        //LeetCode Tasks String
         //LeetCode Task 13. Roman to Integer
         String task13Str = "MCMXCIV";
         out.println("LeetCode Task 13. Roman to Integer:\n" + stringTask.romanToInt(task13Str) + "\n");
@@ -19,5 +22,19 @@ public class Main {
         //LeetCode Task 1108. Defanging an IP Address
         String task1108 = "1.1.1.1";
         out.println("LeetCode Task 1108. Defanging an IP Address:\n" + stringTask.defangIPaddr(task1108) + "\n");
+
+
+        //LeetCode Tasks Algorithms
+        //LeetCode Task 21. Merge Two Sorted Lists
+        algorithmTask.ListNode list1Node3 = new algorithmTask.ListNode(4, null);
+        algorithmTask.ListNode list1Node2 = new algorithmTask.ListNode(2, list1Node3);
+        algorithmTask.ListNode list1Node1 = new algorithmTask.ListNode(1, list1Node2);
+
+        algorithmTask.ListNode list2Node3 = new algorithmTask.ListNode(4, null);
+        algorithmTask.ListNode list2Node2 = new algorithmTask.ListNode(3, list2Node3);
+        algorithmTask.ListNode list2Node1 = new algorithmTask.ListNode(1, list2Node2);
+
+        algorithmTask.printListNode(algorithmTask.mergeTwoLists(list1Node1, list2Node1));
+
      }
 }
---
+++ src/com/Education/algorithmTask.java
@@ -0,0 +1,41 @@
+package com.Education;
+
+public class algorithmTask {
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){}
+        ListNode(int val) { this.val = val;}
+        ListNode(int val, ListNode next){ this.val = val; this.next = next; }
+
+    }
+
+    public static void printListNode(ListNode listNode){
+        if (listNode != null){
+            System.out.print("[" + listNode.val + "] ");
+            printListNode(listNode.next);
+        }
+    }
+
+    /*
+        LeetCode Task 21. Merge Two Sorted Lists
+     */
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode parth = new ListNode();
+        ListNode tail = parth;
+        while (list1 != null && list2 != null){
+            if (list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            }
+            else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = list1 != null ? list1 : list2;
+        return parth.next;
+    }
+}
